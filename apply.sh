537441245dc2f171ec188663a3145e074ae37016
mamonaco1973 <mamonaco1973@gmail.com>
2025-04-15 16:22:18 +0000

    fix 03-deploy step

--- apply.sh
+++ apply.sh
@@ -20,6 +20,19 @@ cd ..
 
 # Step 2 - Build AMIS
 
+
+# Extract password from AWS Secrets Manager
+password=$(aws secretsmanager get-secret-value \
+  --secret-id packer-credentials \
+  --query 'SecretString' \
+  --output text | jq -r '.password')
+
+# Check if the password is empty or null
+if [[ -z "$password" || "$password" == "null" ]]; then
+  echo "ERROR: Failed to retrieve password from secret 'packer-credentials'"
+  exit 1
+fi
+
 vpc_id=$(aws ec2 describe-vpcs --region us-east-2 --filters "Name=tag:Name,Values=packer-vpc" --query "Vpcs[0].VpcId" --output text)
 subnet_id=$(aws ec2 describe-subnets --region us-east-2 --filters "Name=tag:Name,Values=packer-subnet-1" --query "Subnets[0].SubnetId" --output text)
 
@@ -31,11 +44,18 @@ cd 02-packer
 cd linux
 echo "NOTE: Building Linux AMI with Packer."
 packer init ./linux_ami.pkr.hcl
-packer build -var "vpc_id=$vpc_id" -var "subnet_id=$subnet_id" ./linux_ami.pkr.hcl || { echo "NOTE: Packer build failed. Aborting."; exit 1; }
+packer build -var "password=$password" -var "vpc_id=$vpc_id" -var "subnet_id=$subnet_id" ./linux_ami.pkr.hcl || { echo "NOTE: Packer build failed. Aborting."; exit 1; }
 cd ..
 
 cd ..
 
+# Step 3 - Deploy AMIs as EC2 instances
+
+echo "NOTE: Deploying EC2 instances based on the AMIs"
+cd 03-deploy
+terraform init
+terraform apply -auto-approve
+cd ..
 
 
 
