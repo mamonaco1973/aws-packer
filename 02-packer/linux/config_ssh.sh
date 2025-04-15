595879e3191cfd2a83bc51c87966345d4b454269
Mike Monaco <mamonaco1973@gmail.com>
2025-04-15 12:38:32 -0400

    add packer account on linux server

--- 02-packer/linux/config_ssh.sh
+++ 02-packer/linux/config_ssh.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "NOTE: Password is $PACKER_PASSWORD"
+# Create the 'packer' user without a home directory
+sudo useradd -m -s /bin/bash packer
+
+# Set the user's password using chpasswd
+echo "packer:$PACKER_PASSWORD" | sudo chpasswd
+
+# Grant passwordless sudo access
+echo "packer ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/packer
+sudo chmod 440 /etc/sudoers.d/packer
