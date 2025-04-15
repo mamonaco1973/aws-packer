2c6ab05def35292884810cd6c458878250157b36
mamonaco1973 <mamonaco1973@gmail.com>
2025-04-15 12:58:22 +0000

    sync

---
+++ 03-deploy/games_server.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "games_server" {
+  ami           = data.aws_ami.latest_games_ami.id
+  instance_type = "t3.micro"
+
+  subnet_id              = data.aws_subnet.packer_subnet_1.id
+  vpc_security_group_ids = [data.aws_security_group.packer_sg_http.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "games-ec2-instance"
+  }
+}
