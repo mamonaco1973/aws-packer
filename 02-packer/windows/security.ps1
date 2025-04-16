3b21663611937d507edd33a31006c515a61d4681
Mike Monaco <mamonaco1973@gmail.com>
2025-04-16 14:40:48 -0400

    password debug

--- 02-packer/windows/security.ps1
+++ 02-packer/windows/security.ps1
@@ -13,8 +13,8 @@ try {
         exit 1
     }
 
-    Write-Host = "Setting packer password to $envPassword"
-    
+    Write-Host "Setting packer password to $envPassword"
+
     # Convert plain text password to secure string
     $securePassword = ConvertTo-SecureString $envPassword -AsPlainText -Force
 
