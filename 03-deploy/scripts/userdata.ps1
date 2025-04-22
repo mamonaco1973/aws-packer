694de6f2e0ec5d6c6bf3544df59d8aab7e4f76a7
Mike Monaco <mamonaco1973@gmail.com>
2025-04-22 12:15:38 -0400

    add diagrams

--- 03-deploy/scripts/userdata.ps1
+++ 03-deploy/scripts/userdata.ps1
@@ -1,6 +1,6 @@
 <powershell>
 
 powershell.exe -ExecutionPolicy Bypass -File "C:\mcloud\boot.ps1"
-Write-Host "The AMI name is ${ami_name}."
+Write-Output "The AMI name is ${ami_name}."
 
 </powershell>
